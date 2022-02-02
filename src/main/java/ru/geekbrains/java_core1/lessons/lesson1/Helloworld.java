0b70ca4ab2f96b57d6e725399850049e0a75adfd
il-kuliko1 <il-kuliko@rambler.ru>
2022-02-02 22:21:52 +0400

    Initial commit

---
+++ src/main/java/ru/geekbrains/java_core1/lessons/lesson1/Helloworld.java
@@ -0,0 +1,17 @@
+package ru.geekbrains.java_core1.lessons.lesson1;
+
+public class Helloworld {
+     public static void main(String[] args){
+         System.out.println("Govno vonyuchee!!!!!!!!!!!!!!!!");
+         System.out.println("Govno vonyuchee!!!!!!!!!!!!!!!!");
+         System.out.println("Govno vonyuchee!!!!!!!!!!!!!!!!");
+         System.out.println("Govno vonyuchee!!!!!!!!!!!!!!!!");
+
+
+
+     }
+
+
+
+
+}
